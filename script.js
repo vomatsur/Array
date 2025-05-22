decd63e10dde9eff563474e10fad233e6e492076
vomatsur <bahriyorovbahrom@gmail.com>
2025-05-22 16:31:37 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+let users = ["olma",  "nok", "uzum", "gilos"];
+
+alert(users)
+
+
+let tasdiq = confirm("Array qoshmoqchimisz");
+
+
+if (tasdiq) {
+   
+    users.push('kiwi');
+    console.log(users);
+    alert( users);
+} else {
+    console.log(users);
+    alert(users);
+    
+}
